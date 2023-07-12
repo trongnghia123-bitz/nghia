217cf3c671e851f961946147d4406cfdca4252f7
trongnghia123-bitz <139299834+trongnghia123-bitz@users.noreply.github.com>
2023-07-12 09:01:54 +0700

    3

---
+++ client-customer/src/App.js
@@ -0,0 +1,45 @@
+//CLI: npm install axios --save
+/*import axios from 'axios';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Loading...'
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h2>Customer page</h2>
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
+  componentDidMount() {
+    axios.get('/hello').then((res) => {
+      const result = res.data;
+      this.setState({ message: result.message });
+    });
+  }
+}
+export default App;*/
+import './App.css';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import MyProvider from './contexts/MyProvider';
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <BrowserRouter >
+          <Main />
+        </BrowserRouter>
+      </MyProvider>
+    );
+  }
+}
+export default App;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.owgiafk.mongodb.net',
+    DB_USER: 'db_nghia',
+    DB_PASS: 'db_nghia123',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'trongnghianeeee@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'ecnguyhwulqffrvy',
+    JWT_SECRET: '123456',
+    JWT_EXPIRES: '60000000', // in milliseconds
+  };
+  module.exports = MyConstants;
+  
